fe51b71febe5d77a13610da34f9431cb3162b095
shivamjaysawal <l.comshivamsomu123@gmai>
2017-01-11 18:47:33 +0530

    hi shivam

---
+++ app/src/main/java/com/example/design_demo/CommonInfo.java
@@ -0,0 +1,128 @@
+package com.example.design_demo;
+
+public class CommonInfo 
+{
+
+	
+	// Its for Live Path on 16 Server
+	
+/*
+	public static String imei="";
+    public static String WebServicePath="http://103.16.141.16/WebServiceAndroidMeijiSFALive/Service.asmx";
+    public static String VersionDownloadPath="http://103.16.141.16/downloads/";
+	public static String VersionDownloadAPKName="MeijiIndirect.apk";
+	
+	public static String DATABASE_NAME = "DbMeijiSFAApp"; 
+	
+	    
+	public static int DATABASE_VERSIONID = 2;      // put this field value based on value in table on the server
+	public static String AppVersionID = "1.0";   // put this field value based on value in table on the server
+	public static int Application_TypeID = 2; //1=Parag Store Mapping,2=Parag SFA Indirect,3=Parag SFA Direct
+	
+	
+	public static String OrderSyncPath="http://103.16.141.16/ReadXML_MejiSFALive/Default.aspx";
+	public static String ImageSyncPath="http://103.16.141.16/ReadXML_MejiImagesLive/Default.aspx";
+	
+	public static String InvoiceSyncPath="http://103.16.141.16/ReadXML_MejiInvoiceLive/Default.aspx";
+	 */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+// Its for (Staging)Testing  Path on 16 server for SFA
+	
+ /*  public static String imei="";
+    public static String WebServicePath="http://103.16.141.16/WebServiceAndroidMeijiSFAStaging/Service.asmx";
+    public static String VersionDownloadPath="http://103.16.141.16/downloads/";
+	public static String VersionDownloadAPKName="MeijiIndirectStaging.apk";
+	
+	public static String DATABASE_NAME = "DbMeijiSFAApp"; 
+	
+	    
+	public static int DATABASE_VERSIONID = 13;      // put this field value based on value in table on the server
+	public static String AppVersionID = "1.1";   // put this field value based on value in table on the server
+	public static int Application_TypeID = 2; //1=Parag Store Mapping,2=Parag SFA Indirect,3=Parag SFA Direct
+	
+	
+	public static String OrderSyncPath="http://103.16.141.16/ReadXML_MejiSFAStaging/Default.aspx";
+	public static String ImageSyncPath="http://103.16.141.16/ReadXML_MejiImagesStaging/Default.aspx";
+	
+	public static String InvoiceSyncPath="http://103.16.141.16/ReadXML_MejiInvoiceStaging/Default.aspx";
+	  
+
+	*/
+	
+
+	// Its for Development  Path on 16 server for SFA
+		
+	   public static String imei="";
+	    public static String WebServicePath="http://103.16.141.16/WebServiceAndroidMeijiSFADevelopment/Service.asmx";
+	    public static String VersionDownloadPath="http://103.16.141.16/downloads/";
+		public static String VersionDownloadAPKName="MeijiIndirectDev.apk";
+		
+		public static String DATABASE_NAME = "DbMeijiSFAApp"; 
+		
+		    
+		public static int DATABASE_VERSIONID = 2;      // put this field value based on value in table on the server
+		public static String AppVersionID = "1.0";   // put this field value based on value in table on the server
+		public static int Application_TypeID = 2; //1=Parag Store Mapping,2=Parag SFA Indirect,3=Parag SFA Direct
+		
+		
+		public static String OrderSyncPath="http://103.16.141.16/ReadXMLForMejiOutletXMLDevelopment/Default.aspx";
+		public static String ImageSyncPath="http://103.16.141.16/ReadXMLForMejiOutletImagesXMLDevelopment/Default.aspx";
+		
+		public static String InvoiceSyncPath="http://103.16.141.16/ReadXML_MejiInvoice_Test/Default.aspx";
+		 
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		 
+		 
+		
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
